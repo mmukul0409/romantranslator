79f4ab92718b2c22e8b0a9f672f18bf0b703d5aa
mmukul0409 <31900606+mmukul0409@users.noreply.github.com>
2017-09-12 12:29:03 -0700

    RomanNumberTest

---
+++ RomanNumbers.java
@@ -0,0 +1,34 @@
+package Test;
+
+public class RomanNumbers {
+
+
+public static final int[] decimal = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+public static final String[] letters = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
+
+public static String stringToRoman(int num) {
+    String roman = "";
+
+    if (num < 1 || num > 3999) {
+        System.out.println("Invalid roman number value!");
+    }
+
+    while (num > 0) {
+        int maxFound = 0;
+        for (int i=0; i < decimal.length; i++) {
+            if (num >= decimal[i]) {
+                maxFound = i;
+            }
+        }
+        roman += letters[maxFound];
+        num -= decimal[maxFound];
+    }
+
+    return roman;       
+  }
+
+public static void main(String args[]){
+	String roman = RomanNumbers.stringToRoman(154);
+	System.out.println("Roman is ##"+roman);
+}
+}
